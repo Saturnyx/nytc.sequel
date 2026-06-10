2538219476a03b71e56cb13c134728bd03ebb179
FriedCircuits177 <friedcircuits177@gmail.com>
2026-06-10 18:54:57 +0800

    Added daemon functionality to ns_shared.construct_thread()

--- ns_shared/thread_constructor.py
+++ ns_shared/thread_constructor.py
@@ -1,7 +1,9 @@
 import threading
 
 
-def construct_thread(fn) -> threading.Thread:
+def construct_thread(fn, daemon=True) -> threading.Thread:
     cls = fn.__self__.__class__.__name__
 
-    return threading.Thread(target=fn, name=f"{fn.__module__}.{cls}.{fn.__name__}")
+    return threading.Thread(
+        target=fn, name=f"{fn.__module__}.{cls}.{fn.__name__}", daemon=daemon
+    )
